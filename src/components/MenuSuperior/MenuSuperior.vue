5dfabf901eb8ca0d18af585665eeba6c39af9cb4
Marisangela <marisangelalima110@gmail.com>
2024-06-24 01:09:19 -0300

    Uso do vuetify configurado

--- src/components/MenuSuperior/MenuSuperior.vue
+++ src/components/MenuSuperior/MenuSuperior.vue
@@ -1,3 +1,5 @@
 <template>
-    
+    <v-container class="w-100 align-center bg-blue">
+      <p class="text-h1">Home</p>
+    </v-container>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,5 @@
 <template>
-    
+    <v-container class="">
+
+    </v-container>
 </template>
